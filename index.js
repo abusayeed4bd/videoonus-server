655e02974e35f3aba5992b6aea4200fe4cfafd6d
abusayeed4bd <abusayeed4bd@gmail.com>
2022-08-01 01:50:05 +0600

    error solved

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const app = express();
 const server = http.createServer(app);
 // const socket = require("socket.io")
 const io = require("socket.io")(server, {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST']
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
 })
 
 
@@ -15,13 +17,18 @@ const PORT = process.env.PORT || 5000;
 io.on("connection", (socket) => {
     socket.emit("me", socket.id)
 
-    socket.on("disconnet", () => {
+    socket.on("disconnect", () => {
         socket.broadcast.emit("callEnded")
     })
+
     socket.on("callUser", (data) => {
         io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
     })
 
-    socket.on("answerCall", (data) => io.to(data.to).emit("callAccepted"), data.signal)
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal)
+    })
 })
+
+app.get("/", (req, res) => { res.send("welcome to vidoonus") })
 server.listen(PORT, () => console.log("listening server", PORT))
