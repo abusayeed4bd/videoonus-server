237b965c13f0ce214f4d3d5a074b9793920c1767
abusayeed4bd <abusayeed4bd@gmail.com>
2022-08-01 01:23:13 +0600

    call user and answer api added

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
-const socket = require("socket.io")
-const io = socket(server);
+// const socket = require("socket.io")
+const io = require("socket.io")(server, {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+})
+
+
 const PORT = process.env.PORT || 5000;
 
 
@@ -11,7 +16,12 @@ io.on("connection", (socket) => {
     socket.emit("me", socket.id)
 
     socket.on("disconnet", () => {
-
+        socket.broadcast.emit("callEnded")
     })
+    socket.on("callUser", (data) => {
+        io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+    })
+
+    socket.on("answerCall", (data) => io.to(data.to).emit("callAccepted"), data.signal)
 })
 server.listen(PORT, () => console.log("listening server", PORT))
